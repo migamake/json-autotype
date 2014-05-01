55f455761a7a9662ec83ed0d4b6c743eca4d2cb2
Michal J. Gajda <mjgajda@gmail.com>
2014-05-01 16:27:14 +0800

    Many more test examples.
    Some of them still fail.

--- testit.sh
+++ testit.sh
@@ -2,7 +2,8 @@
 
 # TODO: add ParseJSON.hs
 ghc Test.hs && \
-for i in test/*.json; do 
-  ./Test $i > JSONTypes.hs && ghc JSONTypes.hs || exit 1
+for i in test/*.json; do
+  basename $i
+  time ./Test $i > JSONTypes.hs && ghc JSONTypes.hs || exit 1
 done
 echo Finished

5cc6aeda4b9bc361f9556facd82c69111b8c4199
Michal J. Gajda <mjgajda@gmail.com>
2014-10-30 02:38:52 +0800

    Better clean script.

--- clean.sh
+++ clean.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 rm `find . -iname '*.dyn_o' -or -iname '*.dyn_hi' -or -iname '*.o' -or -iname '*.p_o' -or -iname '*.p_hi' -or -iname '*.hi'`
+
+for i in test/*.json; do
+  OUT=`basename $i .json`.hs
+  echo rm -f $OUT
+  rm -f $OUT
+done
+echo Finished

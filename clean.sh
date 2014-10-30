b2acbe1359aa8bda9119f9192d6c26290d2af107
Michal J. Gajda <mjgajda@gmail.com>
2014-10-31 06:30:06 +0800

    Even better cleanup.

--- clean.sh
+++ clean.sh
@@ -4,9 +4,11 @@ rm `find . -iname '*.dyn_o' -or -iname '*.dyn_hi' -or -iname '*.o' -or -iname '*
 
 rm JSONTypes.hs
 
-for i in test/*.json; do
-  OUT=`basename $i .json`.hs
-  echo rm -f $OUT
-  rm -f $OUT
+for EXT in json bad_json auto_error; do 
+  for i in test/*.${EXT}; do
+    OUT=`basename $i .${EXT}`.hs
+    echo rm -f $OUT
+    rm -f $OUT
+  done
 done
 echo Finished

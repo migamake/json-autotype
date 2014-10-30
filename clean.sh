741dae0412bdc9d66c1f3770e25960c320e44b42
Michal J. Gajda <mjgajda@gmail.com>
2014-10-30 08:58:44 +0800

    Slight improvement to cleanup script.

--- clean.sh
+++ clean.sh
@@ -2,6 +2,8 @@
 
 rm `find . -iname '*.dyn_o' -or -iname '*.dyn_hi' -or -iname '*.o' -or -iname '*.p_o' -or -iname '*.p_hi' -or -iname '*.hi'`
 
+rm JSONTypes.hs
+
 for i in test/*.json; do
   OUT=`basename $i .json`.hs
   echo rm -f $OUT

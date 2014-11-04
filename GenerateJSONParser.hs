1f1d65c3ac1f8960a7387e9d93c172555aae4969
Michal J. Gajda <mjgajda@gmail.com>
2014-11-04 15:39:23 +0800

    Added  main parser.

--- GenerateJSONParser.hs
+++ GenerateJSONParser.hs
@@ -2,7 +2,8 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 module Main where
 
-import           System.IO                 (stdin, stderr, stdout, IOMode(..))
+import           System.Exit
+import           System.IO                 (stdin, stderr, stdout, IOMode(..), hPutStrLn)
 import           System.FilePath           (splitExtension)
 import           Control.Monad             (forM_, when)
 import           Control.Exception(assert)
@@ -31,55 +32,97 @@ capitalize input = Text.toUpper (Text.take 1 input)
 
 header :: Text -> Text
 header moduleName = Text.unlines [
-                       "{-# LANGUAGE TemplateHaskell #-}"
-                      ,Text.concat ["module ", capitalize moduleName, " where"]
-                      ,""
-                      ,"import           Data.Text (Text)"
-                      ,"import           Data.Aeson(decode, Value(..), FromJSON(..),"
-                      ,"                            (.:), (.:?), (.!=))"
-                      ,"import           Data.Aeson.TH"
-                      ,""]
+   "{-# LANGUAGE TemplateHaskell #-}"
+  ,Text.concat ["module ", capitalize moduleName, " where"]
+  ,""
+  ,"import           System.Environment (getArgs)"
+  ,"import           Control.Monad      (forM_)"
+  ,"import           Data.ByteString.Lazy.Char8 as BSL"
+  ,"import           Data.Text (Text)"
+  ,"import           Data.Aeson(decode, Value(..), FromJSON(..),"
+  ,"                            (.:), (.:?), (.!=))"
+  ,"import           Data.Aeson.TH"
+  ,""]
+
+epilogue :: Text
+epilogue          = Text.unlines
+  [""
+  ,"parse :: FilePath -> IO TopLevel"
+  ,"parse filename = do result <- decode `fmap` BSL.readFile filename"
+  ,"                    case result of"
+  ,"                      Nothing -> fail $ \"Cannot parse JSON from file: \" ++ filename"
+  ,"                      Just r  -> return r"
+  ,""
+  ,"main :: IO ()"
+  ,"main = do filenames <- getArgs"
+  ,"          forM_ filenames (\\f -> parse f >>= print)"
+  ,""]
 
 -- * Command line flags
-defineFlag "filename"  (defaultOutputFilename :: FilePath) "Write output to the given file"
-defineFlag "suggest"   True                                "Suggest candidates for unification"
-defineFlag "autounify" True                                "Automatically unify suggested candidates"
-defineFlag "fakeFlag"  True                                "Ignore this flag - it doesn't exist!!!"
+defineFlag "outputFilename"  (defaultOutputFilename :: FilePath) "Write output to the given file"
+defineFlag "suggest"         True                                "Suggest candidates for unification"
+defineFlag "autounify"       True                                "Automatically unify suggested candidates"
+defineFlag "fakeFlag"        True                                "Ignore this flag - it doesn't exist!!!"
 
 -- Tracing is switched off:
 myTrace :: String -> IO ()
 myTrace _msg = return ()
 --myTrace = putStrLn 
 
+-- | Report an error to error output.
+report   :: Text -> IO ()
+report    = Text.hPutStrLn stderr
+
+-- | Report an error and terminate the program.
+fatal    :: Text -> IO ()
+fatal msg = do report msg
+               exitFailure
+
+-- | Take a set of JSON input filenames, Haskell output filename, and generate module parsing these JSON files.
+generateHaskellFromJSONs :: [FilePath] -> FilePath -> IO ()
+generateHaskellFromJSONs inputFilenames outputFilename = do
+  assertM (extension == ".hs")
+  withFileOrHandle outputFilename WriteMode stdout $ \hOut ->
+    forM_ inputFilenames $ \inputFilename ->
+      withFileOrHandle inputFilename ReadMode stdin $ \hIn ->
+        -- First we decode JSON input into Aeson's Value type
+        do bs <- BSL.hGetContents hIn
+           Text.hPutStrLn stderr $ "Processing " `Text.append` Text.pack (show moduleName)
+           myTrace ("Decoded JSON: " ++ show (decode bs :: Maybe Value))
+           case decode bs of
+             Nothing -> report $ "Cannot decode JSON input from " `Text.append` Text.pack (show inputFilename)
+             Just v  -> do -- If decoding JSON was successful...
+               -- We start by writing module header
+               Text.hPutStrLn hOut $ header $ Text.pack moduleName
+               -- We extract type structure from the JSON value.
+               let t        = extractType v
+               myTrace $ "type: " ++ show t
+               -- We split different dictionary labels to become different type trees (and thus different declarations.)
+               let splitted = splitTypeByLabel "TopLevel" t
+               myTrace $ "splitted: " ++ show splitted
+               assertM $ not $ any hasNonTopTObj $ Map.elems splitted
+               -- We compute which type labels are candidates for unification
+               let uCands = unificationCandidates splitted
+               myTrace $ "candidates: " ++ show uCands
+               when flags_suggest $ forM_ uCands $ \cs -> do
+                                      putStr "-- "
+                                      Text.putStrLn $ "=" `Text.intercalate` cs
+               -- We unify the all candidates or only those that have been given as command-line flags.
+               let unified = if flags_autounify
+                               then unifyCandidates uCands splitted
+                               else splitted
+               myTrace $ "unified: " ++ show unified
+               -- We write types as Haskell type declarations to output handle
+               Text.hPutStrLn hOut $ displaySplitTypes unified
+               Text.hPutStrLn hOut   epilogue
+  where
+    (moduleName, extension) = splitExtension $
+                                if     outputFilename == "-"
+                                  then defaultOutputFilename
+                                  else outputFilename
+generateJSON _               outputFilename = error "Generating common type from multiple input files is not implemented yet!"
+
 main :: IO ()
 main = do filenames <- $initHFlags "json-autotype -- automatic type and parser generation from JSON"
-          let (moduleName, extension) = splitExtension $
-                                          if flags_filename == "-"
-                                            then defaultOutputFilename
-                                            else flags_filename
-          assertM (extension == ".hs")
           -- TODO: should integrate all inputs into single type set!!!
-          withFileOrHandle flags_filename WriteMode stdout $ \hOut ->
-            forM_ filenames $ \filename ->
-              withFileOrHandle filename ReadMode stdin $ \hIn ->
-                do bs <- BSL.hGetContents hIn
-                   Text.hPutStrLn stderr $ "Processing " `Text.append` Text.pack (show moduleName)
-                   myTrace ("Decoded JSON: " ++ show (decode bs :: Maybe Value))
-                   let Just v   = decode bs
-                   let t        = extractType v
-                   myTrace $ "type: " ++ show t
-                   let splitted = splitTypeByLabel "TopLevel" t
-                   myTrace $ "splitted: " ++ show splitted
-                   Text.hPutStrLn hOut $ header $ Text.pack moduleName
-                   assertM $ not $ any hasNonTopTObj $ Map.elems splitted
-                   let uCands = unificationCandidates splitted
-                   myTrace $ "candidates: " ++ show uCands
-                   when flags_suggest $ forM_ uCands $ \cs -> do
-                                          putStr "-- "
-                                          Text.putStrLn $ "=" `Text.intercalate` cs
-                   let unified = if flags_autounify
-                                   then unifyCandidates uCands splitted
-                                   else splitted
-                   myTrace $ "unified: " ++ show unified
-                   Text.hPutStrLn hOut $ displaySplitTypes unified
-
+          generateHaskellFromJSONs filenames flags_outputFilename
--- testit.sh
+++ testit.sh
@@ -5,10 +5,11 @@ SRC=${EXE}.hs
 
 # TODO: add ParseJSON.hs
 ghc --make ${SRC} -o ${EXE} && \
-for i in test/*.json; do
+for i in test/*.json examples/*.json; do
   basename $i
   echo ./${EXE} $i
   OUT=`basename $i .json`.hs
-  time ./${EXE} $i --filename ${OUT} && ghc ${OUT} || exit 1
+  time ./${EXE} $i --outputFilename ${OUT} && ghc ${OUT} || exit 1
+  runghc ${OUT} ${i} || exit 2
 done
 echo Finished

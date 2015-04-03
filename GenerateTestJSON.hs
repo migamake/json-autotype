8590144f0d113919c02071d229bd84421d72ae98
Michal J. Gajda <mjgajda@gmail.com>
2015-04-03 14:34:37 +0800

    Make sure that test cases generated are unique.

--- GenerateTestJSON.hs
+++ GenerateTestJSON.hs
@@ -5,10 +5,12 @@
 {-# LANGUAGE StandaloneDeriving   #-}
 {-# LANGUAGE ViewPatterns         #-}
 {-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE FlexibleContexts     #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 module Main where
 
 import           Control.Applicative
+import           Control.Monad.State        as State
 import           Data.Maybe
 import           System.Exit
 import           System.IO                 (stdin, stderr, stdout, IOMode(..))
@@ -18,6 +20,7 @@ import           System.Process            (system)
 import           Control.Monad             (forM_, forM, when)
 import qualified Data.ByteString.Lazy.Char8 as BSL
 import qualified Data.HashMap.Strict        as Map
+import qualified Data.Set                   as Set
 import           Data.Aeson
 import           Data.Function             (on)
 import           Data.List
@@ -83,46 +86,81 @@ extractTypeFromJSONFile inputFilename =
                myTrace $ "Type: " ++ pretty t
                return $ Just t
 
---withoutDuplicates gen = undefined
+vectorWithoutDuplicates ::  Ord b => Int -> Gen b -> Gen [b]
+vectorWithoutDuplicates i gen = take i
+                              .  removeDuplicates
+                             <$> infiniteListOf gen
+
+removeDuplicates ::  Ord a => [a] -> [a]
+removeDuplicates list = fst $ filterM checkDup list `runState` Set.empty
+  where
+    checkDup x = do seen <- State.get
+                    if x `Set.member` seen
+                      then
+                        return False
+                      else do
+                        State.put $ x `Set.insert` seen
+                        return True
+
+-- TODO: check for generic Ord?
+instance Ord Value where
+  Null       `compare`  Null      = EQ
+  Null       `compare`  _         = LT
+  _          `compare`  Null      = GT
+  (Bool   a) `compare` (Bool   b) = a `compare` b
+  (Bool   a) `compare`  _         = LT
+  _          `compare` (Bool   b) = GT
+  (Number a) `compare` (Number b) = a `compare` b
+  (Number _) `compare`  _         = LT
+  _          `compare` (Number _) = GT
+  (String a) `compare` (String b) = a `compare` b
+  (String a) `compare` _          = LT
+  _          `compare` (String b) = GT
+  (Array  a) `compare` (Array  b) = a `compare` b
+  (Array  a) `compare` _          = LT
+  _          `compare` (Array  b) = GT
+  (Object a) `compare` (Object b) = Map.toList a `compare` Map.toList b
 
 -- | Take a set of JSON input filenames, Haskell output filename, and generate module parsing these JSON files.
 generateTestJSONs :: IO ()
 generateTestJSONs = do
-    generate
-    results <- forM (zip inputFilenames outputFilenames) $ \(inputFilename, outputFilename) -> do
-      jsonValue :: Value <- generate $ resize flags_size arbitraryTopValue
-      BSL.writeFile inputFilename $ encode jsonValue
-      -- Read type from each file
-      typeForEachFile  <- catMaybes <$> mapM extractTypeFromJSONFile [inputFilename]
-      -- Unify all input types
-      when (null typeForEachFile) $ do
-        report "No valid JSON input file..."
-        exitFailure
-      let finalType = foldr1 unifyTypes typeForEachFile
-      -- We split different dictionary labels to become different type trees (and thus different declarations.)
-      let splitted = splitTypeByLabel "TopLevel" finalType
-      --myTrace $ "SPLITTED: " ++ pretty splitted
-      assertM $ not $ any hasNonTopTObj $ Map.elems splitted
-      -- We compute which type labels are candidates for unification
-      let uCands = unificationCandidates splitted
-      myTrace $ "CANDIDATES:\n" ++ pretty uCands
-      when flags_suggest $ forM_ uCands $ \cs -> do
-                             putStr "-- "
-                             Text.putStrLn $ "=" `Text.intercalate` cs
-      -- We unify the all candidates or only those that have been given as command-line flags.
-      let unified = if flags_autounify
-                      then unifyCandidates uCands splitted
-                      else splitted
-      myTrace $ "UNIFIED:\n" ++ pretty unified
-      -- We start by writing module header
-      writeHaskellModule outputFilename unified
-      if flags_test
-        then do
-          r <- (==ExitSuccess) <$> system (unwords $ ["runghc", outputFilename] ++ inputFilenames)
-          when r $ mapM_ removeFile [inputFilename, outputFilename]
-          return r
-        else
-          return True
+    testValues :: [Value] <- generate $
+                               resize flags_size $
+                                 vectorWithoutDuplicates 100 arbitraryTopValue
+    results               <- forM (zip3 inputFilenames outputFilenames testValues) $
+      \(inputFilename, outputFilename, jsonValue) -> do
+        BSL.writeFile inputFilename $ encode jsonValue
+        -- Read type from each file
+        typeForEachFile  <- catMaybes <$> mapM extractTypeFromJSONFile [inputFilename]
+        -- Unify all input types
+        when (null typeForEachFile) $ do
+          report "No valid JSON input file..."
+          exitFailure
+        let finalType = foldr1 unifyTypes typeForEachFile
+        -- We split different dictionary labels to become different type trees (and thus different declarations.)
+        let splitted = splitTypeByLabel "TopLevel" finalType
+        --myTrace $ "SPLITTED: " ++ pretty splitted
+        assertM $ not $ any hasNonTopTObj $ Map.elems splitted
+        -- We compute which type labels are candidates for unification
+        let uCands = unificationCandidates splitted
+        myTrace $ "CANDIDATES:\n" ++ pretty uCands
+        when flags_suggest $ forM_ uCands $ \cs -> do
+                               putStr "-- "
+                               Text.putStrLn $ "=" `Text.intercalate` cs
+        -- We unify the all candidates or only those that have been given as command-line flags.
+        let unified = if flags_autounify
+                        then unifyCandidates uCands splitted
+                        else splitted
+        myTrace $ "UNIFIED:\n" ++ pretty unified
+        -- We start by writing module header
+        writeHaskellModule outputFilename unified
+        if flags_test
+          then do
+            r <- (==ExitSuccess) <$> system (unwords $ ["runghc", outputFilename] ++ inputFilenames)
+            when r $ mapM_ removeFile [inputFilename, outputFilename]
+            return r
+          else
+            return True
     putStrLn $ "Successfully generated "      ++ show (length results) ++
                " JSON files, out of planned " ++ show flags_count  ++ " cases."
   where

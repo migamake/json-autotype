c95043d5dbecfbce11befb89b5b55cda2626808a
Michal J. Gajda <mjgajda@gmail.com>
2016-03-14 00:36:29 +0800

    Fix test suite.

--- testit.sh
+++ testit.sh
@@ -4,6 +4,8 @@
 EXE=dist/build/json-autotype/json-autotype
 SRC=${EXE}.hs
 
+GHCOPTS=-package=aeson
+#GHCOPTS=-package=aeson-0.9.0.1
 # TODO: add ParseJSON.hs
 #ghc --make ${SRC} -o ${EXE} && \
 #cabal build
@@ -11,7 +13,7 @@ for i in `find test/ examples/ -iname '*.json'`; do
   basename $i
   echo ./${EXE} $i
   OUT=`basename $i .json`.hs
-  time ./${EXE} $i --outputFilename ${OUT} && ghc  -package=aeson-0.9.0.1 ${OUT} || exit 1
-  runghc -package=aeson-0.9.0.1 ${OUT} ${i} || exit 2
+  time ./${EXE} $i --outputFilename ${OUT} && cabal exec ghc -- ${GHCOPTS} ${OUT} || exit 1
+  cabal exec runghc -- ${GHCOPTS} ${OUT} ${i} || exit 2
 done
 echo Finished

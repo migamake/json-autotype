75b723dcfa49566fa1b03d16196d8c6ff86dd3e5
Peter Simons <simons@cryp.to>
2018-02-12 10:02:07 +0100

    test: fix broken TestExamples suite
     - The logic that generated the capitalized outputFilename didn't cope with the
       fact that those files are located in a sub-directory.
    
     - When a test case failed, it was hard to determine which one that was because
       the test suite just kept running. Instead of running all tests regardless of
       success or failure, we now abort the test suite with a meaningful error
       message the moment we encounter the first failed test.
    
     - Make 'capitalize' a total function.

--- test/TestExamples.hs
+++ test/TestExamples.hs
@@ -1,13 +1,13 @@
 -- Test over all files in examples/ directory
 module Main(main) where
 
-import Control.Monad(forM)
+import Control.Monad(forM, forM_, unless)
 import Data.Char(toUpper)
 import Data.Functor ((<$>))
 import Data.List(isPrefixOf, isSuffixOf)
 import System.Directory(doesDirectoryExist, getDirectoryContents)
-import System.FilePath(dropExtension, (</>), (<.>))
-import System.Exit(exitSuccess, exitWith, ExitCode(..))
+import System.FilePath((</>), (<.>), takeBaseName, replaceFileName)
+import System.Exit(ExitCode(..))
 
 import CommonCLI
 
@@ -29,25 +29,18 @@ getRecursiveContents topdir = do
         else return []
 
 capitalize :: String -> String
+capitalize [] = []
 capitalize (s:ss) = toUpper s:ss
 
 main :: IO ()
 main  = do
   filenames <-  filter (isSuffixOf ".json")
             <$> getRecursiveContents "examples"
-  results   <- forM filenames $ \filename -> do
-    let outputFilename = capitalize (dropExtension filename) <.> "hs"
+  forM_ filenames $ \filename -> do
+    let outputFilename = filename `replaceFileName` capitalize (takeBaseName filename <.> "hs")
     genResult <- runghc ["GenerateJSONParser.hs", filename, "--outputFilename", outputFilename]
-    return 0
-    if genResult == ExitSuccess
-      then return 0 -- number of failures so far
-      else do
-        parserResult <- runghc [outputFilename, filename]
-        if parserResult == ExitSuccess
-           then return 0
-           else return 1
-  exitCode $ sum results
-
-exitCode  :: Int -> IO ()
-exitCode 0 = exitSuccess
-exitCode n = exitWith $ ExitFailure n
+    unless (genResult == ExitSuccess) $
+      fail (unwords ["test case", show filename, "failed with", show genResult])
+    parserResult <- runghc [outputFilename, filename]
+    unless (parserResult == ExitSuccess) $
+      fail (unwords ["generated parser", show outputFilename, "failed with", show parserResult])

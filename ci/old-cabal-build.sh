bbd0a7ea7d818424e470593d7a83595922617dcc
Michal J. Gajda <mjgajda@migamake.com>
2020-03-10 16:55:42 +0100

    Fix old cabal builds.

--- ci/old-cabal-build.sh
+++ ci/old-cabal-build.sh
@@ -2,20 +2,28 @@
 
 source ci/common.sh
 
-message "Dependencies"
-cabal update
-cabal install --dependencies-only --enable-tests
+#message "Dependencies"
+#cabal update
+#cabal install --dependencies-only --enable-tests
 
-message "Build"
-cabal v1-configure --enable-tests --allow-newer
-cabal v1-build
-cabal v1-test
+for PKG in json-alt run-haskell-module; do
+  (cd ${PKG};
+   message "Build $PKG";
+   cabal v1-configure --enable-tests --allow-newer;
+   cabal v1-build;
+   cabal v1-test
+  )
+done;
 
-message "Prepare release artifacts"
-mkdir -p bin sdist
-cabal install --bindir=bin/
-cabal sdist   --builddir=sdist/
-cabal haddock --builddir hackage-docs --for-hackage
+for PKG in json-alt run-haskell-module; do
+  (cd ${PKG};
+   message "Prepare release artifacts for ${PKG}"
+   mkdir -p bin sdist
+   cabal install --bindir=../bin/
+   cabal sdist   --builddir=../sdist/
+   cabal haddock --builddir ../hackage-docs --for-hackage
+  )
+done;
 
-message "Run on own source"
-bin/homplexity-cli lib/
+message "Run on example"
+bin/json-autotype json-autotype/test/colors.json

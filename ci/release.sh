9e61157d4d6732d3fec28b348d247aaf0bed1fe4
Michal J. Gajda <mjgajda@migamake.com>
2020-03-17 02:22:00 +0100

    Fixing release step.

--- ci/release.sh
+++ ci/release.sh
@@ -5,11 +5,11 @@ source ci/common.sh
 message "Packages"
 for FILENAME in sdist/*.tar.gz; do
   message "Uploading ${FILENAME}"
-  cabal upload ${*} --username="$HACKAGE_USER" --password="$HACKAGE_PASSWORD" $i;
+  cabal upload ${*} --username="$HACKAGE_USER" --password="$HACKAGE_PASSWORD"    "${FILENAME}";
 done
 
 message "Documentation"
 for FILENAME in hackage-docs/*.tar.gz; do
   message "Uploading ${FILENAME}"
-  cabal upload ${*} --username="$HACKAGE_USER" --password="$HACKAGE_PASSWORD" -d $i;
+  cabal upload ${*} --username="$HACKAGE_USER" --password="$HACKAGE_PASSWORD" -d "${FILENAME}";
 done

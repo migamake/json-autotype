dab6db549de28491af11a64dc0915873e1507bbf
Michal J. Gajda <mjgajda@migamake.com>
2020-03-17 09:49:14 +0100

    Fixing tarball test.

--- ci/package-test.sh
+++ ci/package-test.sh
@@ -15,5 +15,10 @@ done
 
 message "Running executable"
 ls -alth bin
-ls -alth json-autotype*
+ls -alth -L bin
+ls -alth json-autotype*/test
+echo bin/${EXEC_NAME} json-autotype-*/test/colors.json
 bin/${EXEC_NAME} json-autotype-*/test/colors.json
+bin/${EXEC_NAME} --version
+cabal exec ${EXEC_NAME} --version
+cabal exec ${EXEC_NAME} json-autotype/test/colors.json

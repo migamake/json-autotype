9cb34e9eb4f7ba2864475c45e76128c41289d135
Michal J. Gajda <mjgajda@migamake.com>
2020-03-21 13:05:40 +0100

    Fixing CI package test.

--- ci/new-cabal-build.sh
+++ ci/new-cabal-build.sh
@@ -2,13 +2,13 @@
 
 source ci/cabal-common.sh
 
-mkdir -p hackage-docs bin sdist
+mkdir -p hackage-docs bin new-bin sdist
 
 message "Build and installation"
 # This is still buggy in Cabal 2.4.x
 #cabal new-install --bindir=bin/ json-autotype
 # Should work for Cabal 3.2
-cabal new-install --installdir=bin/ json-autotype
+cabal new-install --installdir=new-bin/ json-autotype
 ls -lR bin
 
 message "Preparing source distribution"
@@ -21,6 +21,8 @@ message "Run on example"
 ls -lR bin/
 # Need working runModule yet!
 #bin/json-autotype json-autotype/test/colors.json --outputFilename=Colors.hs
-bin/json-autotype json-autotype/test/colors.json
+new-bin/json-autotype json-autotype/test/colors.json
 
+mkdir bin
+cp --dereference new-bin/json-autotype bin/json-autotype
 

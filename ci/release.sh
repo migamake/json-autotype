551219d1a0d9518ba6e402e585eba2355423fd83
Michal J. Gajda <mjgajda@migamake.com>
2020-03-22 14:29:21 +0100

    More lenient release script.

--- ci/release.sh
+++ ci/release.sh
@@ -5,11 +5,11 @@ source ci/common.sh
 message "Packages"
 for FILENAME in sdist/*.tar.gz; do
   message "Uploading ${FILENAME}"
-  cabal upload ${*} --username="$HACKAGE_USER" --password="$HACKAGE_PASSWORD"    "${FILENAME}";
+  cabal upload ${*} --username="$HACKAGE_USER" --password="$HACKAGE_PASSWORD"    "${FILENAME}" || echo "Already uploaded? ${FILENAME}";
 done
 
 message "Documentation"
 for FILENAME in hackage-docs/*.tar.gz; do
   message "Uploading ${FILENAME}"
-  cabal upload ${*} --username="$HACKAGE_USER" --password="$HACKAGE_PASSWORD" -d "${FILENAME}";
+  cabal upload ${*} --username="$HACKAGE_USER" --password="$HACKAGE_PASSWORD" -d "${FILENAME}" || echo "Already uploaded? ${FILENAME}";
 done
